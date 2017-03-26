0b7a37e176087fb02998ed71f605eb57701ef4b3
Molybden91 <amodenov@yandex.ru>
2017-03-26 16:52:46 +0300

    portion reading
    Added portion reading, instead of data preparation. Lest speed (6s for 300mb), but lesser code(algorithm) complexity.

--- LogAnalyzer/src/LogAnalyzer.java
+++ LogAnalyzer/src/LogAnalyzer.java
@@ -1,10 +1,10 @@
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.BufferUnderflowException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.*;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 /**
@@ -12,34 +12,42 @@ import java.util.logging.Logger;
  */
 
 /**
- *
+ * Main class for analyzing log files.
  */
 public class LogAnalyzer {
+    //private field- instance of Logger class
     private static Logger log=Logger.getLogger(LogAnalyzer.class.getName());
     private static int timeDigits=3;//hours(1 value)+minuted(1 value)+seconds and it's fractions(1 value)=3
-    private ArrayList<Long> inputTime=new ArrayList<>();
-    private ArrayList<Long> outputTime=new ArrayList<>();
-    private ArrayList<Integer> timeDelays=new ArrayList<>();
-    private ArrayList<Integer> outputTypes=new ArrayList<>();
-    private ArrayList<Long> inputID=new ArrayList<>();
-    private ArrayList<Long> outputID=new ArrayList<>();
-    public void addToInputTime(long value){
-        inputTime.add(value);
-    }
-    public void addToOutputTime(long value){
-        outputTime.add(value);
-    }
-    public void addToOutputTypes(int type){outputTypes.add(type);}
-    public void addToInputID(long id){inputID.add(id);}
-    public void addToOutputID(long id){outputID.add(id);}
     public int getTimeDigits() {
         return timeDigits;
     }
-
     /**
-     *
-     * @param s
-     * @return
+     * Auxiliary method to convert read and translated in microseconds times back in a representation typical of the input file.
+     * @param time- time value to convert
+     * @return time in String form
+     */
+    public static String ConvertTimeFromMks(long time){
+        long hours=(long) (time/Math.pow(10,6)/3600);
+        long minutes=(long)((time-hours*Math.pow(10,6)*3600)/Math.pow(10,6)/60);
+        double seconds=(time-hours*Math.pow(10,6)*3600-minutes*Math.pow(10,6)*60)/Math.pow(10,6);
+        String forHours=""+hours;
+        String forMinutes=""+minutes;
+        String forSeconds=""+seconds;
+        if(hours<10){
+            forHours="0"+forHours;
+        }
+        if(minutes<10){
+            forMinutes="0"+forMinutes;
+        }
+        if(seconds<10){
+            forSeconds="0"+forSeconds;
+        }
+        return forHours+":"+forMinutes+":"+forSeconds;
+    }
+    /**
+     * Method to convert hexadecimal number in form of string in form of decimal long number.
+     * @param s- input number in hex form
+     * @return decimal number converted from hexadecimal
      */
     public static long hex2decimal(String s) {
         String digits = "0123456789ABCDEF";
@@ -50,14 +58,138 @@ public class LogAnalyzer {
             int d = digits.indexOf(c);
             val = 16*val + d;
         }
+        log.fine("hexadecimal number was converted to decimal");
         return val;
     }
+
     /**
-     *
-     * @param s
-     * @return
+     * Method that helps to load data from initial log file. Uses fastest methodology- file mapping through NIO library.
+     * @param fileName- the name of input file
+     * @return list of strings contained in the initial file
+     * @throws IOException
      */
-    public long findTime(String s){
+    public static ArrayList<StringBuilder> loadFile(String fileName)throws IOException{
+        long start=new Date().getTime();
+        log.info("Starting to load date from input file:"+fileName);
+        File file=new File(fileName);
+        FileChannel fileChannel=new RandomAccessFile(file,"r").getChannel();
+        MappedByteBuffer buffer=fileChannel.map(FileChannel.MapMode.READ_ONLY,0,fileChannel.size());
+        log.info("Buffer capacity:"+buffer.capacity());
+        ArrayList <StringBuilder> result=new ArrayList<>();
+        StringBuilder tmp=new StringBuilder();
+        try {
+            while (buffer.hasRemaining()){
+                byte read= buffer.get();
+                if((char)read=='\n'){
+                    result.add(tmp);
+                    tmp=new StringBuilder();
+                }else {
+                    tmp.append((char) read);
+                }
+            }
+        }catch (BufferUnderflowException e){
+            log.log(Level.SEVERE,"BufferUnderFlowException:"+Thread.currentThread().getStackTrace());
+            System.out.println("BufferUnderFlowException:");
+        }finally {
+            fileChannel.close();
+        }
+        long end=new Date().getTime();
+        log.info("Data was loaded. Consumed time in second:"+(double)(end-start)/1000);
+        return result;
+    }
+    /**
+     * Primary method for operation with data.  The basic algorithm is based on the assumption, that every second must need
+     * to start from input. So if we have one output which does not fit in a second,  we will add it to the set of considered
+     * outputs and for this reason, sometimes considered interval can include a little more time than one second. In addition,
+     * we considered time delays so that if some input doesn't have output in the analyzed time interval it will not be
+     * taken into account.
+     * It invokes loadFile(fileName) method as well as auxiliary methods: findTime, findId, findType.
+     */
+    public static void dataOperation(String fileName){
+        try {
+            long start=new Date().getTime();
+            log.info("Starting to operating with date from input file:"+fileName);
+            LogAnalyzer.writeTextToFile("output.csv","");
+            ArrayList<StringBuilder> stringBuilders = loadFile(fileName);
+            ArrayList<Long> inputTimeSec=new ArrayList<>();
+            ArrayList<Long> outputTimeSec=new ArrayList<>();
+            ArrayList<Integer> outputTypesSec=new ArrayList<>();
+            LinkedHashSet <Integer> outputTypesSet=new LinkedHashSet<>();
+            ArrayList<Long> inputIdSec=new ArrayList<>();
+            ArrayList<Long> outputIdSec=new ArrayList<>();
+            ArrayList<Long> timeDelaysSec=new ArrayList<>();
+            for(Iterator<StringBuilder> iter=stringBuilders.iterator();iter.hasNext();){
+                String current=iter.next().toString();
+                if(!current.contains("SQLProxy")&&!current.contains("P2_COD")) {
+                    if(inputTimeSec.size()==0){
+                        inputTimeSec.add(findTime(current));
+                        inputIdSec.add(findId(current));
+                    }else {
+                        if (findTime(current) - inputTimeSec.get(0) <= 1000000) {
+                            if (current.contains("input")) {
+                                inputTimeSec.add(findTime(current));
+                                inputIdSec.add(findId(current));
+                            }
+                            if (current.contains("output")) {
+                                outputTimeSec.add(findTime(current));
+                                outputIdSec.add(findId(current));
+                                outputTypesSec.add(findType(current));
+                                outputTypesSet.add(findType(current));
+                            }
+                        } else {
+                            if (current.contains("output")) {
+                                outputTimeSec.add(findTime(current));
+                                outputIdSec.add(findId(current));
+                                outputTypesSec.add(findType(current));
+                                outputTypesSet.add(findType(current));
+                            }
+                            ArrayList<Integer> tmpTypes = new ArrayList<>();
+                            ArrayList<Long> tmpDelays = new ArrayList<>();
+                            for (int i = 0; i < inputIdSec.size(); i++) {
+                                for (int j = 0; j < outputIdSec.size(); j++) {
+                                    if (inputIdSec.get(i).equals(outputIdSec.get(j))) {
+                                        timeDelaysSec.add(outputTimeSec.get(j) - inputTimeSec.get(i));
+                                        tmpTypes.add(outputTypesSec.get(j));
+                                    }
+                                }
+                            }
+                            for (Integer type : outputTypesSet) {
+                                for (int i = 0; i < tmpTypes.size(); i++) {
+                                    if (tmpTypes.get(i).equals(type)) {
+                                        tmpDelays.add(timeDelaysSec.get(i));
+                                    }
+                                }
+                                Operations operations = new Operations(tmpDelays);
+                                String stringForWrite=LogAnalyzer.ConvertTimeFromMks(inputTimeSec.get(0)) + "," + type + "," + inputTimeSec.size() + "," + operations.getAverage() + "," +
+                                        operations.getMedian() + "," + operations.getPercentile90() + "," + operations.getPercentle99() + "," + operations.getMax();
+                                LogAnalyzer.writeTextToFileWithoutRewrite("output.csv",stringForWrite);
+                            }
+                            tmpTypes.clear();
+                            tmpDelays.clear();
+                            timeDelaysSec.clear();
+                            outputTypesSet.clear();
+                            outputTypesSec.clear();
+                            outputIdSec.clear();
+                            inputIdSec.clear();
+                            inputTimeSec.clear();
+                            outputTimeSec.clear();
+                        }
+                    }
+                }
+            }
+            long end=new Date().getTime();
+            log.info("Data was handled. Consumed time in second:"+(double)(end-start)/1000);
+        }catch (IOException e){
+            log.log(Level.SEVERE, "IOException:"+Thread.currentThread().getStackTrace());
+            System.out.println("IOException");
+        }
+    }
+    /**
+     * Method to find and collect time of input or output from String
+     * @param s- initial String to find time
+     * @return time of operation in form of long number
+     */
+    public static long findTime(String s){
         double [] time=new double[timeDigits];
         char [] input=s.toCharArray();
         double timeValue=0;
@@ -73,16 +205,16 @@ public class LogAnalyzer {
             tmp.append(Character.toString(input[i]));
         }
         timeValue=(timeValue+Double.parseDouble(tmp.toString()))*1000000;
-
+        log.fine("Iteration for time searching was performed");
         return (long) timeValue;
     }
 
     /**
-     *
-     * @param s
-     * @return
+     * Method to find and collect type from String
+     * @param s- initial String to find type
+     * @return type of operation in form of int number
      */
-    public int findType(String s){
+    public static int findType(String s){
         char [] currentString=s.toCharArray();
         StringBuilder result=new StringBuilder();
         int i=s.indexOf("type")+5;
@@ -94,14 +226,15 @@ public class LogAnalyzer {
             }
             i++;
         }
+        //log.fine("Iteration for type searching was performed");
         return Integer.parseInt(result.toString());
     }
     /**
-     *
-     * @param s
-     * @return
+     * Method to find and collect id of operation from String
+     * @param s- initial String to find id
+     * @return id of operation in form of long number
      */
-    public long findId(String s){
+    public static long findId(String s){
         char [] currentString=s.toCharArray();
         StringBuilder result=new StringBuilder();
         int i=s.indexOf("id")+3;
@@ -113,64 +246,66 @@ public class LogAnalyzer {
             }
             i++;
         }
-
+        log.fine("Iteration for id searching was performed");
         return hex2decimal(result.toString());
     }
-    /**
-     *
-     */
-    public void calculateTimeDelays(){
-        for(int i=0;i<inputTime.size();i++){
-        }
-    }
-    /**
-     *
-     * @param fileName
-     */
-    public void getData(String fileName){
-        try {
-            Scanner scanner=new Scanner(new FileInputStream(fileName));
-            while (true) {
-                String currentInputString = scanner.nextLine();
-                if(!scanner.hasNext()){
-                    break;
-                }
-                if(currentInputString.contains("input")){
-                    addToInputID(findId(currentInputString));
-                    addToInputTime(findTime(currentInputString));
-                }
-                if(currentInputString.contains("output")){
-                    addToOutputID(findId(currentInputString));
-                    addToOutputTime(findTime(currentInputString));
-                    addToOutputTypes(findType(currentInputString));
-                }
 
+    /**
+     *
+     * @param fileName is name of file in which you want to put portion of data string by string without rewriting.
+     * @param text is the text that will be put in file.
+     */
+    public static void writeTextToFileWithoutRewrite(String fileName,String text){
+        File file=new File(fileName);
+        log.fine("writing iteration to file: "+fileName);
+        try { if(!file.exists()) throw new FileNotFoundException();
+            FileWriter outfile=new FileWriter(file.getAbsoluteFile(),true);
+            try{
+                outfile.write(text+"\r\n");
+            }finally {
+                outfile.close();
             }
         }catch (IOException e){
-            log.log(Level.SEVERE, "IOException:"+Thread.currentThread().getStackTrace());
-            System.out.println("IOException");
+            throw  new RuntimeException(e);
         }
     }
 
     /**
-     *
-     * @param inputFileName
+     * Method, that writes a String in the certain file.
+     * @param fileName is name of file in which you want to put data.
+     * @param text is the text that will be put in file.
      */
-    public static void makeTable(String inputFileName){
-
+    public static void writeTextToFile(String fileName,String text){
+        File file=new File(fileName);
+        log.fine("Writing to file: "+fileName);
+        try { if (!file.exists()){
+            file.createNewFile();
+        }
+            PrintWriter out=new PrintWriter(file.getAbsoluteFile());
+            try {
+                out.print(text);
+            }finally {
+                out.close();
+            }
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
-    public static void main(String [] args)throws IOException{
-        LogAnalyzer analyzer=new LogAnalyzer();
-        //Scanner scanner=new Scanner(new FileInputStream("input.log"));
-        //String s=scanner.nextLine();
-        System.out.println(new Date());
-        analyzer.getData("input.log");
-        System.out.println(new Date());
-        System.out.println(analyzer.inputTime.get(0));
-        //analyzer.addToInputTime(analyzer.findTime(s));
-        //double [] doubles=analyzer.inputTime.get(0);
-        //System.out.println(doubles);
-        //analyzer.addToOutputTypes(analyzer.findType(s));
-        //System.out.println(analyzer.outputTypes);
+
+    /**
+     * The main method contains entry point for logger and for calculations. It invokes only one method- dataOperation.
+     * @param args
+     */
+    public static void main(String [] args){
+        try {
+            LogManager.getLogManager().readConfiguration(LogAnalyzer.class.getResourceAsStream("logging.properties"));
+        }catch (IOException e){
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+        long start=new Date().getTime();
+        dataOperation("input.log");
+        long end=new Date().getTime();
+        System.out.println("Consumed time in seconds="+(double)(end-start)/1000);
+
     }
 }
--- LogAnalyzer/src/Operations.java
+++ LogAnalyzer/src/Operations.java
@@ -6,23 +6,65 @@ import java.util.Collections;
 /**
  * Created by Pahan on 23.03.2017.
  */
+
+/**
+ * Class containing simple tools to calculate parameters for output file
+ */
 public class Operations {
-    static int [] array;
-    Operations(ArrayList<Integer> integers){
-        array=new int[integers.size()];
-        for(int i=0;i<integers.size();i++){
-            array[i]=integers.get(i);
+    static long [] array;
+    //Constructor takes input list of values, than sort it. Each operation for this instance will be held for this data.
+    Operations(ArrayList<Long> integers){
+        if(integers.size()==0){
+            array=new long[1];
+            array[0]=0;
+        }else {
+            array=new long[integers.size()];
+            for (int i = 0; i < integers.size(); i++) {
+                array[i] = integers.get(i);
+            }
         }
         Arrays.sort(array);
     }
-    public static int getMedian(){
+
+    /**
+     * Method for determining median value from initial sorted array of values.
+     * @return median value for handled array in form of long value.
+     */
+    public  long getMedian(){
         return array[array.length/2];
     }
-    public static double getPercentile90(){
+
+    /**
+     * Method for determining 90th percentile value from initial sorted array of values.
+     * @return 90th percentile for handled array in form of long value.
+     */
+    public  long getPercentile90(){
         return array[(int) (array.length*0.9)];
     }
-    public static double getPercentle99(){
+
+    /**
+     * Method for determining 90th percentile value from initial sorted array of values.
+     * @return 99th percentile for handled array in form of long value.
+     */
+    public  long getPercentle99(){
         return array[(int)(array.length*0.99)];
     }
 
+    /**
+     * Method for determining average value from initial sorted array of values.
+     * @return average value for handled array in form of long value.
+     */
+    public double getAverage(){
+        int summ=0;
+        for(int i=0;i<array.length;i++){
+            summ+=array[i];
+        }
+        return summ/array.length;
+    }
+
+    /**
+     * Method for determining maximum value from initial sorted array of values.
+     * @return maximum value for handled array in form of long value.
+     */
+    public long getMax(){return array[array.length-1];}
 }
